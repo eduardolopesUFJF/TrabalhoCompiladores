f4a1520ea6c08a4546e4e39426bcc404b45e5dfe
eduardolopesufjf <eduardolopes@ice.ufjf.br>
2018-06-17 11:50:43 -0300

    versao final ajustada

--- AnaliseSintatica/src/analisesintatica/AnaliseSintatica.java
+++ AnaliseSintatica/src/analisesintatica/AnaliseSintatica.java
@@ -15,33 +15,34 @@ public class AnaliseSintatica {
         String path;
         if (args.length > 0) {
             path = args[0];
-            try {
-                ANTLRFileStream input = new ANTLRFileStream(path);
-
-                PortugaLexer lexer = new PortugaLexer(input);
-                CommonTokenStream tokens = new CommonTokenStream(lexer);
-                PortugaParser parser = new PortugaParser(tokens);
-
-                //Remove o tratamento padrao, pra evitar duplicacao de mensagem
-                parser.removeErrorListeners();
-
-                //Adiciona nosso Listener
-                MyError errorListener = new MyError();
-                parser.addErrorListener(errorListener);
-                ParseTree tree = parser.program();
-
-                //Verifica se houve algum erro, e se sim throw antes de criar o viewer.
-                if (!errorListener.errors.isEmpty()) {
-                    throw new RuntimeException(String.join("\n", errorListener.errors));
-                }
-
-                TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
-                viewer.open();
-            } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
-            }
         } else {
-            System.out.println("Você precisa informar um arquivo com o código fonte a ser analisado.");
+            path = "input.txt";
+        }
+
+        try {
+            ANTLRFileStream input = new ANTLRFileStream(path);
+
+            PortugaLexer lexer = new PortugaLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            PortugaParser parser = new PortugaParser(tokens);
+
+            //Remove o tratamento padrao, pra evitar duplicacao de mensagem
+            parser.removeErrorListeners();
+
+            //Adiciona nosso Listener
+            MyError errorListener = new MyError();
+            parser.addErrorListener(errorListener);
+            ParseTree tree = parser.program();
+
+            //Verifica se houve algum erro, e se sim throw antes de criar o viewer.
+            if (!errorListener.errors.isEmpty()) {
+                throw new RuntimeException(String.join("\n", errorListener.errors));
+            }
+
+            TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
+            viewer.open();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
     }
 
